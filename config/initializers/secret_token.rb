64b2fd7dcebfc11ff2d2f791308cb82a3feea3fb
Leo Allen <leo.f.allen@gmail.com>
2012-09-22 12:29:47 +0100

    Adding rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FarmFinder::Application.config.secret_token = '91ff3c6c56ef813a2f4943e76dc7730aaf7220e8d3baa558f4b9cb912187ba4ff6a2c57bc23fc5b587607a4c42730b95aa5ec52dd4a33f67b0c8b640da4ba945'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FarmFinder::Application.config.session_store :cookie_store, key: '_farm_finder_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FarmFinder::Application.config.session_store :active_record_store
